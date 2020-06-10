f293ffc2d39b6e5f4a621fdce3e950c67b129492
phantomAvenger <63266282+phantomAvenger@users.noreply.github.com>
2020-06-10 12:34:49 -0700

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,8 @@ $(function(){
         var username = $('#username').val();
         var content = $('#content').val();
         var avatar = $('#avatar').val();
-        if (link==null || link=="",content==null || content=="")
-        {
-            alert("Please Fill Out All The Fields");
+        if (link==null || link=="",content==null || content=="") {
+            alert("Are you actually stupid? Fill out the inputs, godammit.");
             return false;
         }
         $.post(link, {"content": content, "username": username, "avatar_url": avatar,});
